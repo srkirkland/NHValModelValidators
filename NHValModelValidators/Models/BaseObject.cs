be2288ba93302ba18ad47edb1e9ecd34cc05826b
Scott Kirkland <srkirkland@gmail.com>
2010-02-18 10:18:40 -0800

    Added NHVal reference and the customer model that validation will act on

---
+++ NHValModelValidators/Models/BaseObject.cs
@@ -0,0 +1,7 @@
+namespace NHValModelValidators.Models
+{
+    public class BaseObject
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ NHValModelValidators/Models/Customer.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using NHibernate.Validator.Constraints;
+
+namespace NHValModelValidators.Models
+{
+    public class Customer : BaseObject
+    {
+        [NotNullNotEmpty]
+        [Length(10)]
+        public virtual string CompanyName { get; set; }
+
+        [NotNullNotEmpty]
+        public virtual string ContactName { get; set; }
+
+        [NotNullNotEmpty]
+        public virtual string Country { get; set; }
+
+        [NotNullNotEmpty]
+        public virtual string Fax { get; set; }
+
+        [Range(1, 200, "Must be between 1 and 200")]
+        public virtual int Age { get; set; }
+
+        public static IList<Customer> GetCustomers(int n)
+        {
+            var customers = new List<Customer>();
+
+            for (int i = 0; i < n; i++)
+            {
+                customers.Add(new Customer { Id = i + 1, CompanyName = "Company" + i, ContactName = "Contact" + i, Country = "USA", Fax = "Fax" });
+            }
+
+            return customers;
+        }
+    }
+}
