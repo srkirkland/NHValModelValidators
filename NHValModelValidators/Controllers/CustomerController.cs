5d7051ee90c149209d6c5a9c662b8d0c174068ab
Scott Kirkland <srkirkland@gmail.com>
2010-02-18 10:26:47 -0800

    Added customer controller and NHVal engine lazy factory

---
+++ NHValModelValidators/Controllers/CustomerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+using NHValModelValidators.Models;
+
+namespace NHValModelValidators.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        public ActionResult Index()
+        {
+            var customers = Customer.GetCustomers(10);
+
+            return View(customers);
+        }
+
+        //
+        // GET: /Customer/Create
+        public ActionResult Create()
+        {
+            var customer = new Customer();
+
+            return View(customer);
+        } 
+
+        //
+        // POST: /Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            TempData["Message"] = "Customer Is Valid: " + customer.IsValid();
+
+            return View(customer);
+        }
+    }
+}
--- NHValModelValidators/Models/BaseObject.cs
+++ NHValModelValidators/Models/BaseObject.cs
@@ -3,5 +3,10 @@ namespace NHValModelValidators.Models
     public class BaseObject
     {
         public int Id { get; set; }
+
+        public bool IsValid()
+        {
+            return ValidatorEngineFactory.ValidatorEngine.IsValid(this);
+        }
     }
 }
